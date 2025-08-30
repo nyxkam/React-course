f18f33eb08f4bf7d088f15616546c99ff780c541
AlexRosas <rosasrias82@gmail.com>
2025-08-29 21:11:54 -0500

    feat: class05-useState
    Handling the useState Hook and what is batching.

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,25 @@ function App() {
   // 1 - mount 
   // 2 - cambio de estado 
   // 3 - async
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState("Alex")
 
+  // batching 
   const countMore = () => {
     setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
   }
-
-  const changeName = () => {
-    setName("Rosas")
-  }
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <Button label={`Count is ${count}`} parentMethod={countMore} />
-      <p>{name}</p>
-      <Button label="Change Name" parentMethod={changeName} />
     </>
   )
 }
