95574c67893222d9ccbf91c7e463dade9c7b4688
AlexRosas <rosasrias82@gmail.com>
2025-08-30 19:21:48 -0500

    feat: class-07 custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,25 @@
-import { useEffect, useState } from 'react'
 import './App.css'
+import { useFetch } from './hooks';
 
+const url = "https://api.example.com/data";
+
+interface Data {
+  name: string;
+  lastName: string;
+  age: number;
+}
 
 function App() {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState("")
-
-  const consoleLoader = (loadingValue: boolean) => {
-    setLoading(loadingValue)
-    console.info(loading)
-  }
-
-  const fetchData = async () => {
-    consoleLoader(true)
-    try {
-      const response = await fetch("https://api.example.com/data")
-
-      if (!response.ok) {
-        throw new Error("Error al obtener datos")
-      }
-
-      const jsoData = await response.json()
-      setData(jsoData)
-    } catch (err) {
-      setError(err as string)
-    } finally {
-      consoleLoader(false)
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
+  const { data, loading, error } = useFetch<Data>(url)
 
   if (loading) {
     return <div>Cargando ... </div>
   }
+
   if (error) {
-    return <div>UPS! Hay un error: {error}</div>
+    return <div>UPS! Hay un error: {error.message}</div>
   }
+
   return (
     <div>{JSON.stringify(data)}</div>
   )
