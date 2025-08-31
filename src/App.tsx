ff533e78fc8550d83becdb403ee65e21ea9c64fa
AlexRosas <rosasrias82@gmail.com>
2025-08-30 19:33:37 -0500

    feat: add user example

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,19 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+const url = "https://api.example.com/data";
+// User ejemplo 
+// const userUrl = "https://api.example.com/user";
+
+interface Data {
+  name: string;
+  lastName: string;
+  age: number;
+}
 
 function App() {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState("")
-
-  const consoleLoader = (loadingValue: boolean) => {
-    setLoading(loadingValue)
-    console.info(loading)
-  }
-
-  const fetchData = async () => {
-    consoleLoader(true)
-    try {
-      const response = await fetch("https://api.example.com/data")
-
-      if (!response.ok) {
-        throw new Error("Error al obtener datos")
-      }
-
-      const jsoData = await response.json()
-      setData(jsoData)
-    } catch (err) {
-      setError(err as string)
-    } finally {
-      consoleLoader(false)
-    }
-  }
-
-  // endpoint:(entidad externa al componente)
-  // Comunicarnos con un endpoint - entidad externa al componente
-
-  // useEffect es un metodo que acepta metodos y un arreglo de dependencias
-  // maneja el ciclo de vida de un componente no tanto asi 
-  // se maneja segun el re-render de un componente
-
-  // El useEffect sirve para sincronizar con entidades externas (sync)
-  // operaciones async 
-  // parametros de entrada
-  // context (depende)
-  // redux (depende)
-  useEffect(() => {
-    fetchData()
-    // lógica ? que lógica ? cuándo se ejecuta esta logica?
-    // 1 - Se ejecuta cuando se monsta el componente
-    // 2 - Cada vez que se modifique uno de los valores del state
-    // la folowing -> el return se ejecuta cada vez que el componente muera o se destruya
-    return () => {
-      // manejar el estado de la memoria
-    }
-  }, [fetchData])
+  const { data, loading, error } = useFetch<Data>(url)
+  //const { data: dataUser, loading: loadingUser; error: errorUser } = useFetch<[name: string]>(url)
 
   if (loading) {
     return <div>Cargando ... </div>
